1340b1ad335442c8426371a46ea463adc5671046
CodeByPrime <mrcody497@gmail.com>
2025-11-11 09:32:43 +0500

    feat:add analytics api

--- controller/url.controller.js
+++ controller/url.controller.js
@@ -1,16 +1,25 @@
-const {nanoid}=require("nanoid")
-const urlModel=require("../model/url.model")
-const mongoose=require("mongoose")
+const { nanoid } = require("nanoid");
+const urlModel = require("../model/url.model");
+const mongoose = require("mongoose");
 
-async function Handlegenerateshorturl(req,res) {
-    const body=req.body;
-    if(!body.url) return res.status(400).json({message:"url is required"})
-    const shortId=nanoid(9);
-    await urlModel.create({
-        shortId: shortId,
-        redirectUrl:body.redirectUrl,
-        visithistory:[]
-    })
-    res.status(201).json({shortUrl:`http://localhost:3000/${shortId}`})
+async function Handlegenerateshorturl(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ message: "url is required" });
+  const shortId = nanoid(9);
+  await urlModel.create({
+    shortId: shortId,
+    redirectUrl: body.url,
+    visithistory: [],
+  });
+  res.status(201).json({ shortUrl: `http://localhost:3000/${shortId}` });
 }
-module.exports={Handlegenerateshorturl}
+
+async function Handlegetanalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await urlModel.findOne({ shortId });
+  return res.json({
+    totalclicks: result.visithistory.length,
+    analytics: result.visithistory,
+  });
+}
+module.exports = { Handlegenerateshorturl ,Handlegetanalytics};
