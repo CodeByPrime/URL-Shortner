5ce0912a9f27d25421604539151907ed860d4d5f
CodeByPrime <mrcody497@gmail.com>
2025-11-11 09:28:30 +0500

    feat:add routes

--- routes/url.route.js
+++ routes/url.route.js
@@ -1,7 +1,8 @@
 const express=require("express")
-const {Handlegenerateshorturl}=require("../controller/url.controller")
+const {Handlegenerateshorturl,Handlegetanalytics}=require("../controller/url.controller")
 
 const app=express()
 const router=express.Router()
-router.post("/",Handlegenerateshorturl(req,res))
+router.post("/",Handlegenerateshorturl)
+router.get("/analytics/:shortId",Handlegetanalytics)
 module.exports=router;
